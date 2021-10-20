1ba54ee6ff5786d38f44e2e394407a065f5d68b7
WellesleyMussolini <iei.muss@gmail.com>
2021-10-20 14:33:51 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import * as Routes from './constants/routes';
 import './App.css';
+import Loader from 'react-loader-spinner';
+
+const Home = lazy(() => import("./pages/Home"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Suspense fallback={
+          <Loader
+            type="Oval"
+            color="#00BFFF"
+            height={80}
+            width={80}
+            style={{
+              height: '100vh',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+          />
+        }>
+          <Switch>
+            <Route path={Routes.HOME} component={Home} exact />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
+      </Router>
     </div>
   );
 }
